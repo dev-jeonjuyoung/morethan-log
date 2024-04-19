bb45f5835478e1cd18f06acbc556034b5333b188
jeonjuyoung <dev.jeonjuyoung@outlook.kr>
2024-04-20 00:29:51 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -53,7 +53,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: false,
+    enable: true,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
@@ -61,10 +61,10 @@ const CONFIG = {
     },
   },
   cusdis: {
-    enable: true,
+    enable: false,
     config: {
       host: "https://cusdis.com",
-      appid: "99f5ea21-17b8-4271-b474-15205112293a", // Embed Code -> data-app-id value
+      appid: "", // Embed Code -> data-app-id value
     },
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
